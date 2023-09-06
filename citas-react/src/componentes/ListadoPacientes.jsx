a75a123b73b3ffc850c8f143a83d74b62afe0e41
FranRodrigueez <122549243+FranRodrigueez@users.noreply.github.com>
2023-09-06 21:01:27 +0200

    21:01 06/09/2023

--- citas-react/src/componentes/ListadoPacientes.jsx
+++ citas-react/src/componentes/ListadoPacientes.jsx
@@ -5,7 +5,7 @@ const ListadoPacientes = () => {
                 <div className="md:w-1/2 lg:w-3/5">
 
                         <h2 className="font-black text-3xl text-center">Listado Pacientes</h2> 
-                        <p className="text-xl mt-5 mb-7 text-center">
+                        <p className="text-xl mt-5 mb-10 text-center">
                                 Administra tus {""}
                                 <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
                         </p>
